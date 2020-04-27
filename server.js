df0350ad5affde3988a0f4cb73ba2dcc552e4763
Glitch (hello-sqlite) <none>
2020-04-27 21:42:52 +0000

    📚🚧 Checkpoint ./server.js:15577456/604 ./package.json:15577456/28

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const bot_data = require("./bot_data");
@@ -32,3 +36,14 @@ client.on("ready", () => {
   console.log("Joined the fray.");
 });
 client.login(bot_data.token);
+
+
+/* Keep alive */
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
