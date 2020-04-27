10dc1e8182e7585fe59e2202ecc5bbb7f1447f35
Glitch (hello-sqlite) <none>
2020-04-27 14:28:58 +0000

    🍎📚 Checkpoint ./dbmodule.js:15577456/1191 ./server.js:15577456/117

---
+++ dbmodule.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+// init sqlite db
+const dbFile = "./.data/sqlite.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(dbFile);
+
+// if ./.data/sqlite.db does not exist, create it
+db.serialize(() => {
+  if (!exists) {
+    db.run(
+      "CREATE TABLE Dreams (id INTEGER PRIMARY KEY AUTOINCREMENT, dream TEXT)"
+    );
+    console.log("New table Dreams created!");
+
+    // insert default dreams
+    db.serialize(() => {
+      db.run(
+        'INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")'
+      );
+    });
+  } else {
+    console.log("DB Loaded");
+  }
+});
+
+exports.f = function() {
+  
+}
