b6b9bcc6ae09358a9e8ec803f3ac8dc51a7b1952
Glitch (hello-sqlite) <none>
2020-04-27 15:16:04 +0000

    🐊🏕 Checkpoint ./dbmodule.js:15577456/361

--- dbmodule.js
+++ dbmodule.js
@@ -12,20 +12,30 @@ const db = new sqlite3.Database(file, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREA
   console.log("Connected to the chinook database.");
 });
 // create info table
-db.run("CREATE TABLE info(id text)", (err) => {
-  if (err) {
-    console.err(err.message);
+db.run(`
+    CREATE TABLE IF NOT EXISTS
+    info(
+    id NOT NULL,
+    text
+  )`, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("info table is created.")
   }
-  console.log("info table is created.")
-});
+);
 
 // add 
 exports.add_info = function(id, text) {
   if (!id || !text) {
-    console.err("invalid input");
+    console.error("Invalid arguments, please refer to the help command");
   }
   
-  db.run("INSERT INTO info(id text) VALUES ${id} ${text}", (err) => {
+  db.run(`
+    INSERT INTO info(id text)
+    VALUES
+    ('${id}', '${text}')
+  `, (err) => {
     if (err) {
       console.err(err.message);
     }
