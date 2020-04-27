f8ebf49e9605a66274c2a5e63d44791f2e19e082
Glitch (hello-sqlite) <none>
2020-04-27 15:28:04 +0000

    🚍👽 Checkpoint ./dbmodule.js:15577456/360

--- dbmodule.js
+++ dbmodule.js
@@ -1,50 +1,67 @@
 const fs = require("fs");
 
+const info_table_name = "info";
+
 /* init sqlite db */
 const file = "./.data/sqlite.db";
 const exists = fs.existsSync(file);
 const sqlite3 = require("sqlite3").verbose();
+
 // open file
 const db = new sqlite3.Database(file, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE , (err) => {
   if (err) {
     console.error(err.message);
   }
-  console.log("Connected to the chinook database.");
+  console.log("Connected to the database.");
 });
+
 // create info table
 db.run(`
     CREATE TABLE IF NOT EXISTS
     info(
-    id NOT NULL,
-    text
-  )`, (err) => {
+      id NOT NULL,
+      text
+    )
+  `, (err) => {
     if (err) {
       console.error(err.message);
     }
-    console.log("info table is created.")
+    console.log("${info_table_name} table is created.")
   }
 );
 
-// add 
+// add function
 exports.add_info = function(id, text) {
   if (!id || !text) {
     console.error("Invalid arguments, please refer to the help command");
   }
   
   db.run(`
-    INSERT INTO info(id text)
+    INSERT INTO ${info_table_name}(id text)
     VALUES
-    ('${id}', '${text}')
+      ('${id}', '${text}')
   `, (err) => {
     if (err) {
-      console.err(err.message);
+      console.error(err.message);
     }
   });
+  return;
 };
 
-// remove
+// remove function
 exports.remove_info = function(id) {
   if (!id) {
     console.err("Invalid arguments, please refer to the help command");
   }
+  
+  db.run(`
+    DELETE FROM ${info_table_name}
+    WHERE
+      id = '${id}'
+  `, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+  });
+  return;
 }
