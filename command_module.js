b9b3985c481b2603ed0e640c25b73baf73a93b7a
Glitch (hello-sqlite) <none>
2020-04-27 18:18:15 +0000

    🦁💷 Checkpoint ./command_module.js:15577456/926

--- command_module.js
+++ command_module.js
@@ -11,8 +11,13 @@ const get_command = function(str) {
 };
 exports.get_command = get_command;
 
-// output error in console
-const
+// output error if exists
+const output_error = function(err) {
+  if (err) {
+    return console.error(err);
+  }
+  return;
+}
 
 
 /* Commands */
@@ -20,19 +25,30 @@ var commands = {};
 
 // ping command
 const ping = function(message, command_content) {
-  //command_content ignore
-  return message.channel.send("pong");
+  // command_content ignore
+  return message.channel.send("pong", output_error);
 }
 commands["ping"] = ping;
 
 // what's command
 const whats = function(message, command_content) {
-  //command_content = info id
+  // command_content = info id
   return dbmodule.get_info(command_content, (err, row) => {
     if (err) {
-      return message.channel.send
+      return message.channel.send("cannot get info: " + command_content, output_error);
     }
+    return message.channel.send(row.text, output_error);
   });
 }
+commands["whats"] = whats;
+
+// add text to database
+const add_info = function(message, command_content) {
+  // command_content[0] = row id
+  // command_content[1] = row text
+  var args = command_content.match("(\S*)\s*(.*)");
+  return dbmodule.add_info(args[1], args[2], output_error);
+}
+commands["add_info"] = add_info;
 
 exports.commands = commands;
