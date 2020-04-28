7cde2c09b080caca0150951e6716081e9c172ed5
Glitch (hello-sqlite) <none>
2020-04-28 18:17:47 +0000

    🌎🏵 Checkpoint ./modules/dbmodule.js:15577456/1696

--- modules/dbmodule.js
+++ modules/dbmodule.js
@@ -21,13 +21,24 @@ const db = new sqlite3.Database(file, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREA
   console.log("Connected to the secret service.");
 });
 
+// Temp to clear all data
+db.run(`
+  DROP TABLE IF EXISTS balance;
+`, [], output_error);
+db.run(`
+  DROP TABLE IF EXISTS items;
+`, [], output_error);
+db.run(`
+  DROP TABLE IF EXISTS inventories;
+`, [], output_error);
+
 
 const init = function() {
   // create balances table to store amount of money users have
   db.run(`
     CREATE TABLE IF NOT EXISTS balances (
       [user_id] INT8 NOT NULL UNIQUE,
-      [balance] INT8 DEFAULT 0 ON CONFLICT REPLACE,
+      [balance] INT8 DEFAULT 0,
       PRIMARY KEY (user_id)
     )
   `, [], output_error);
@@ -36,8 +47,8 @@ const init = function() {
   db.run(`
     CREATE TABLE IF NOT EXISTS items (
       [item_id] INT8 NOT NULL UNIQUE,
-      [name] NVARCHAR[32] NOT NULL ON CONFLICT REPLACE,
-      [description] NVARCHAR[128] DEFAULT "" ON CONFLICT REPLACE,
+      [name] NVARCHAR[32] NOT NULL,
+      [description] NVARCHAR[128] DEFAULT "",
       PRIMARY KEY (item_id)
     )
   `, [], output_error);
@@ -47,7 +58,7 @@ const init = function() {
     CREATE TABLE IF NOT EXISTS inventories (
       [user_id] INT8 NOT NULL,
       [item_id] INT8 NOT NULL,
-      [amount] INT8 DEFAULT 0 ON CONFLICT REPLACE,
+      [amount] INT8 DEFAULT 0,
       PRIMARY KEY (user_id, item_id),
       FOREIGN KEY (item_id) REFERENCES items (item_id)
         ON DELETE CASCADE,
@@ -75,8 +86,10 @@ exports.get_balance = get_balance;
 // set a player's balance
 const set_balance = function(user_id, value, callback) {
   return db.run(`
-    INSERT INTO balances (user_id, balance)
+    INSERT OR REPLACE
+    INTO balances (user_id, balance)
     VALUES (?, ?)
+    
   `, [user_id, value], callback);
 }
 exports.set_balance = set_balance;
@@ -99,20 +112,19 @@ const get_inventory = function(user_id, callback) {
     WHERE user_id = ?
   `, [user_id], callback);
 }
+exports.get_inventory = get_inventory;
 
-// remove info using an id from table
-exports.remove_info = function(id, callback) {
-  if (!id) {
-    console.err("Invalid arguments, please refer to the help command");
-  }
-  
+// sets data for an inventory item for one player
+const set_inventory_item = function(user_id, item_id, amount, callback) {
   return db.run(`
-      DELETE FROM ${info_table_name}
-      WHERE
-        id = ?
-    `, [id], callback
-  );
-};
+    INSERT OR REPLACE
+    INTO inventories
+    WHERE (
+      user_id = ?
+      item_id = ?
+    )
+  `)
+}
 
 // get all tuples in info
 exports.get_all = function(callback) {
