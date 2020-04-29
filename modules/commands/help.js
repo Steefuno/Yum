5f77cebba73a61e8b5e79b523ca879155be4d683
Glitch (hello-sqlite) <none>
2020-04-29 18:35:28 +0000

    🏢🌦 Checkpoint ./modules/commands/market.js:15577456/897 ./modules/commands/help.js:15577456/272

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -12,20 +12,24 @@ const output_error = function(err) {
 
 // displays list of commands
 exports.func = function(message, command_content) {
-  var embed = new Discord.MessageEmbed()
-    .setTitle("Help Menu")
-    .setDescription("\
-      The prefix is currently **" + prefix + "**\n\
-      " + prefix + " SOME_COMMAND - gives more info on a command\n\
-      " + prefix + " help - opens this help menu\n\
-      " + prefix + " bal - displays how much money you have\n\
-      " + prefix + " credits - shows anyone that helped with the game\
-    ")
-    .setFooter(message.author.username + "#" + message.author.discriminator)
-    .setColor(6611350)
-  ;
-  
-  return message.channel.send("", embed, output_error);
+  if (command_content[2].length == 0) {
+    var embed = new Discord.MessageEmbed()
+      .setTitle("Help Menu")
+      .setDescription("\
+        The prefix is currently **" + prefix + "**\n\
+        " + prefix + " SOME_COMMAND - gives more info on a command\n\
+        " + prefix + " help - opens this help menu\n\
+        " + prefix + " bal - displays how much money you have\n\
+        " + prefix + " credits - shows anyone that helped with the game\
+      ")
+      .setFooter(message.author.username + "#" + message.author.discriminator)
+      .setColor(6611350)
+    ;
+
+    return message.channel.send("", embed, output_error);
+  } else {
+    
+  }
 }
 
 exports.help = function(message, command_content) {
--- modules/commands/market.js
+++ modules/commands/market.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const bot_data = require("./../bot_data");
-const dbmodule = require("./../dbmodule");
+
+const prefix = bot_data.prefix;
 
 const output_error = function(err) {
   if (err) {
@@ -9,35 +10,15 @@ const output_error = function(err) {
   return;
 }
 
-// displays caller's balance
+// displays functions in market module
 exports.func = function(message, command_content) {
-  var description = "The prefix is currently " + prefix;
   
-  var embed = new Discord.MessageEmbed()
-    .setTitle("Monies")
-    .setFooter(message.author.username + "#" + message.author.discriminator)
-    .setColor(6611350)
-  ;
-  
-  dbmodule.get_balance(message.author.id, (err, row) => {
-    if (err) {
-      return console.error(err);
-    }
-    
-    // credit to elise naming monies as "epic gamer coins"
-    if (row) {
-      embed = embed.setDescription("You have " + row.balance + " " + bot_data.currency); 
-    } else {
-      embed = embed.setDescription("You have 0 " + bot_data.currency);
-    }
-    return message.channel.send("", embed, output_error);
-  });
 }
 
 exports.help = function(message, command_content) {
   var embed = new Discord.MessageEmbed()
     .setTitle("Help Balance")
-    .setDescription("This is the command to see how much money you have.")
+    .setDescription("This is the module to access the market functions.")
     .setFooter(message.author.username + "#" + message.author.discriminator)
     .setColor(6611350)
   ;
