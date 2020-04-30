c29e1d925d2a5ebc4d371396c43fd2295a3fb8c3
Glitch (hello-sqlite) <none>
2020-04-30 01:17:19 +0000

    👰💾 Checkpoint ./modules/commands/balance.js:15577456/448 ./modules/commands/modules/market/view.js:15577456/780

--- modules/commands/balance.js
+++ modules/commands/balance.js
@@ -13,18 +13,16 @@ const output_error = function(err) {
 
 // displays caller's balance
 exports.func = function(message, command_content) {
-  var description = "The prefix is currently " + prefix;
-  
-  var embed = new Discord.MessageEmbed()
-    .setTitle("Monies")
-    .setFooter(message.author.username + "#" + message.author.discriminator)
-    .setColor(6611350)
-  ;
-  
   dbmodule.get_balance(message.author.id, (err, row) => {
     if (err) {
       return console.error(err);
     }
+    
+    var embed = new Discord.MessageEmbed()
+      .setTitle("Monies")
+      .setFooter(message.author.username + "#" + message.author.discriminator)
+      .setColor(6611350)
+    ;
     
     // credit to elise naming monies as "epic gamer coins"
     if (row) {
--- modules/commands/modules/market/view.js
+++ modules/commands/modules/market/view.js
@@ -11,6 +11,20 @@ const output_error = function(err) {
 
 exports.func = function(message, command_content) {
   return market.get_catalog((catalog) => {
-    
+    var embed = new Discord.MessageEmbed()
+      .setTitle("Help Menu")
+      .setDescription("\
+        The prefix is currently **" + prefix + "**\n\
+        " + prefix + " SOME_COMMAND - gives more info on a command\n\
+        " + prefix + " help - opens this help menu\n\
+        " + prefix + " bal - displays how much money you have\n\
+        " + prefix + " market - displays market commands\n\
+        " + prefix + " credits - shows anyone that helped with the game\
+      ")
+      .setFooter(message.author.username + "#" + message.author.discriminator)
+      .setColor(6611350)
+    ;
+
+    return message.channel.send("", embed, output_error);
   });
 }
