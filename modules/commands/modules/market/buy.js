c13eb0cbec014442335500e9791c2868a07dbc57
Glitch (hello-sqlite) <none>
2020-04-30 03:49:17 +0000

    ⛑🍮 Checkpoint ./modules/commands/modules/market/buy.js:15577456/2235

--- modules/commands/modules/market/buy.js
+++ modules/commands/modules/market/buy.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
+const bot_data = require("./../../../bot_data");
 const dbmodule = require("./../../../dbmodule");
 const market =  require("./../../market");
+
+var currency = bot_data.currency;
 
 const output_error = function(err) {
   if (err) {
@@ -37,8 +40,33 @@ exports.func = function(message, command_content) {
       // if item is being sold today, deduct money and purchase
       if (catalog[i][0] == item_id) {
         // get user's money
-        return dbmodule.get_balance(message.author.id, (balance) => {
+        return dbmodule.get_balance(message.author.id, (err, row) => {
+          if (err) {
+            console.error("Can't get balance of " + message.author.id);
+            return message.reply("oops, something went wrong! You didn't lose " + currency + ", but you didn't buy the item either.", output_error);
+          }
           
+          var balance;
+          if (row != null) {
+            balance = row.balance;
+          } else {
+            balance = 0;
+          }
+          
+          // if not enough money
+          if (balance < catalog[i][2] * amount) {
+            return message.reply("sorry, you need more " + currency + ".", output_error);
+          }
+          
+          // if enough money, deduct
+          return dbmodule.set_balance(message.author.id, balance - (catalog[i][2] * amount), (err) => {
+            if (err) {
+              console.error("Can't set balance of " + message.author.id);
+              return message.reply("oops, something went wrong! You didn't lose " + currency + ", but you didn't buy the item either.", output_error);
+            }
+            
+            
+          })
         });
       }
     }
